803da18952994a6ca14329aa9471e7ae5c666b33
MuriloSouza955 <souza.murilo@gmail.com>
2025-04-24 20:39:24 -0300

    05-01 Funções

---
+++ script.js
@@ -0,0 +1,5 @@
+/*
+O que são funções?
+Nesta aula, expliquei o conceito de funções, que são blocos de código para realizar tarefas específicas. As funções podem ser chamadas, executadas e invocadas várias vezes. Elas ajudam a organizar o código, tornando-o mais compreensível e reutilizável. Cada função tem uma responsabilidade, facilitando a compreensão do código. As funções são como botões em um controle, cada um com uma função específica. Elas permitem reutilizar o código e separar as responsabilidades.
+
+*/
