6129d0500ae31ec1adc4560e3fcadc637b0ff4ca
MuriloSouza955 <souza.murilo@gmail.com>
2025-04-24 21:31:19 -0300

    05-03 Argumentos e Parâmetros

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 /*
-Primeira função
-Neste vídeo, é ensinado como criar uma função em programação. A declaração da função é feita com a palavra "function" seguida do nome desejado. O escopo da função é delimitado por chaves e tudo dentro delas é o que a função executará. Após definir a mensagem desejada, a função é chamada pelo nome seguido de parênteses. A função pode ser chamada várias vezes sem a necessidade de recriá-la, reaproveitando seu conteúdo.
+Argumentos e Parâmetros
+Nesta aula, expliquei sobre parâmetros e argumentos em funções. Os parâmetros são variáveis que a função recebe, enquanto os argumentos são os valores passados para a função. Mostrei exemplos práticos, como uma função de mensagem e uma função de soma. Destaquei a importância da ordem dos parâmetros e como definir valores padrão para eles. Expliquei que a ordem dos parâmetros é crucial, mas é possível definir argumentos padrão. Esses conceitos são essenciais para tornar as funções mais dinâmicas.
 
 */
 
-function message(){
-    console.log("Olá, é bom ter voce aqui!");
+function message(userName){
+    console.log("Olá "+userName+", é bom ter você aqui!" );
 }
 
-message();
-message();
-message();
+message("João");
+message("Maria");
+message("Pedro");
+
+function sum(a, b){
+    return a + b;
+}
+
+console.log(sum(1, 2));
+console.log(sum(10, 12));
+console.log(sum(15, 23));
+
+function joinText(text1="", text2="", text3=""){
+    console.log(text1 + text2 + text3)
+}
+
+joinText("Murilo", " Souza!");
+joinText("Murilo", " Souza", " é um programador!");
