da91c63fd794b0812b7b176d91d6311513a1cd6b
MuriloSouza955 <souza.murilo@gmail.com>
2025-04-25 21:57:48 -0300

    05-05 Escopo de Função

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 /*
-Retornando valores
-Quando se cria uma função em programação, é possível executar instruções e cálculos dentro dela. Pode-se utilizar a palavra-chave "return" para retornar um valor específico da função. Dessa forma, é possível utilizar o resultado fora da função, atribuindo-o a uma variável ou exibindo-o diretamente com o console.log. O "return" permite que o valor calculado dentro da função seja acessado e utilizado em outras partes do código.
+Escopo de Função
+Neste vídeo, expliquei sobre o comportamento de Hosting das funções em JavaScript. Mostrei como é possível utilizar uma função antes mesmo de declará-la, devido ao fato de que as funções são içadas para o topo. Também demonstrei como é possível criar funções dentro de funções e como o escopo influencia na execução das mesmas. É importante entender esses conceitos para evitar erros ao trabalhar com funções em JavaScript.
+
+
 
 */
 
+showMessage("Olá, Murilo! antes da função declarada");
+
+
+//exemplo de hoisting, onde a função é declarada depois de ser chamada
+function showMessage(message){
+    console.log(message);
+    endline()
+
+    function endline(){
+        console.log("--------------------------------");
+    }
+}
+
+showMessage("Olá, Murilo! após a função declarada");
+
+
+
+//exemplo de função dentro de função
 function sum(a, b){
-    return a + b;
+    function sum(a, b){
+        return a + b;
+    }
+    return sum(a, b);
 }
 
 console.log(sum(1, 2));
 console.log(sum(10, 12));
 console.log(sum(15, 23));
+
